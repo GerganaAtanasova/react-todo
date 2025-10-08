86684ebe6afec00cd838fde757fbcddffcf4bbb3
Gergana Atanasova <gergana.atanasova3333@gmail.com>
2025-10-08 13:15:00 +0300

    -Add bootstrap -Add create/remove item functionality

---
+++ src/components/TodoTable.js
@@ -0,0 +1,30 @@
+import TodoRowItem from "./TodoRowItem";
+
+function TodoTable(props) {
+    const todos = props.todos;
+
+    return (
+        <table className="table table-hover">
+            <thead>
+            <tr>
+                <th scope='col'>#</th>
+                <th scope='col'>Description</th>
+                <th scope='col'>Assigned</th>
+            </tr>
+            </thead>
+            <tbody>
+            {todos.map(todo => (
+                <TodoRowItem
+                    key={todo.rowNumber}
+                    rowNumber={todo.rowNumber}
+                    rowDescription={todo.rowDescription}
+                    rowAssigned={todo.rowAssigned}
+                    deleteTodo={props.deleteTodo}
+                />
+            ))}
+            </tbody>
+        </table>
+    )
+}
+
+export default TodoTable;
